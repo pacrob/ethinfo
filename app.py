3ee3e5b277efffaf817ceeff9325fd50954a6b5d
pacrob <paul@pacrob.com>
2021-11-30 16:54:37 -0700

    show current price and recent info from the chain

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import config
+import ccxt
+import time
 from web3 import Web3
 from flask import Flask, render_template
 
@@ -7,7 +9,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "EthInfo!"
+    binance = ccxt.binance() 
+    eth_price = binance.fetch_ticker('ETH/USDC')
+    latest_block = w3.eth.get_block('latest')
+
+    latest_blocks = []
+    for block_number in range(latest_block.number, latest_block.number-10, -1):
+        block = w3.eth.get_block(block_number)
+        latest_blocks.append(block)
+
+    latest_transactions = []
+    for tx in latest_blocks[-1]['transactions'][-10:]:
+        transaction = w3.eth.get_transaction(tx)
+        latest_transactions.append(transaction)
+
+    current_time = time.time()
+
+    return render_template("index.html", 
+                            eth_price=eth_price,
+                            latest_blocks=latest_blocks,
+                            latest_transactions=latest_transactions,
+                            current_time=current_time)
 
 @app.route("/tx/<tx_hash>")
 def transaction(tx_hash):
